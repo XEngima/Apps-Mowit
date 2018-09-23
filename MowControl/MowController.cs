2b55f9dacf4227034d9f18d773c749be2a52a42d
Daniel Eiserman <daniel.eiserman@protonmail.com>
2018-09-23 20:26:52 +0200

    New version

--- MowControl/MowController.cs
+++ MowControl/MowController.cs
@@ -13,7 +13,7 @@ namespace MowControl
     {
         private bool _mowerIsHome;
 
-        public static string Version { get { return "1.33"; } }
+        public static string Version { get { return "1.35"; } }
 
         public MowController(
             IMowControlConfig config,
